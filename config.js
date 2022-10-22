e88887e6ba1cdd940240af6be02f1f3309e6585c
STIFF404 <free92001@gamil.com>
2022-10-22 13:26:54 +0700

    botmd

---
+++ config.js
@@ -0,0 +1,80 @@
+/** 
+
+Yg Mau Sv Nomerku?
+
+wa.me/6285875158363
+
+Minimal Kasih Cr kalo mau reupload😡
+Create By Dika Ardnt.
+Recode By Naze & Stiff Totemo
+
+Kalau Sampai Ketemu Lagi Creator Bot
+Yg Reupload Tanpa Kasih Cr 
+Sc Akan Saya Encode
+
+Base : Hisoka
+Recode : Naze & Stiff Totemo
+
+**/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['6283879685072']
+global.ownernomer = "6283879685072"
+global.premium = ['6283879685072']
+global.packname = 'Stiff No Counter'
+global.author = ''
+global.sessionName = 'session'  //jangan diganti bro nanti error
+global.jumlha = '999'
+global.jumhal = '100000000000000'
+global.jumlah = '1000000000'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: '✅Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Anjasss Tunggu Aja Ya🗿',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 1000
+}
+global.thumb = fs.readFileSync('./media/image/naze.jpg')
+global.faall = fs.readFileSync('./media/image/fake.jpg')
+global.mygit = 'https://github.com/STIFF404'
+global.myyt = 'https://youtube.com/channel/UCp3wAViqPkz7NN1fSENHSqA'
+global.myytv = 'https://youtube.com/shorts/GJ79IoNsptc?feature=share'
+global.mygc = "https://chat.whatsapp.com/B6ol9DdGElJHUogjtYB02D"
+global.botname = 'Loli Bot'
+global.akulaku = 'No Account'
+global.ytname = 'Stiff Totemo'
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
